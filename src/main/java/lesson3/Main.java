2a722346ebb53293e9e4a99b34ad9cefb2f24b80
OGMOL <olegmolokov11@gmail.com>
2024-11-03 18:56:16 +0300

    task3 completed

--- src/main/java/lesson3/Main.java
+++ src/main/java/lesson3/Main.java
@@ -3,6 +3,8 @@ package lesson3;
 public class Main {
     public static void main(String[] args) {
         printThreeWords();
+        checkSumSing();
+        printColor();
     }
 
     /**
@@ -19,13 +21,27 @@ public class Main {
     public static void checkSumSing() {
         int a = -2;
         int b = -3;
-
         int sum = a + b;
 
         if (sum >= 0) {
             System.out.println("Сумма положительная");
         } else {
             System.out.println("Сумма отрицательная");
+        }
+    }
+
+    /**
+     * Метод выводит цвет в зависимости от значения переменной value.
+     */
+    public static void printColor() {
+        int value = 150;
+
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value > 0 && value <= 100) {
+            System.out.println("Желтый");
+        } else {
+            System.out.println("Зеленый");
         }
     }
 }
