cc57ba0a1ae6cbd95939cb6ee79a8fd9a0964b2c
OGMOL <olegmolokov11@gmail.com>
2024-11-03 17:06:53 +0300

    Task2 completed

--- src/main/java/lesson3/Main.java
+++ src/main/java/lesson3/Main.java
@@ -11,4 +11,21 @@ public class Main {
     public static void printThreeWords() {
         System.out.println("Orange" + "\n" + "Banana" + "\n" + "Apple");
     }
+
+    /**
+     * Метод проверяет сумму двух переменных и выводит,
+     * положительная или отрицательная сумма.
+     */
+    public static void checkSumSing() {
+        int a = -2;
+        int b = -3;
+
+        int sum = a + b;
+
+        if (sum >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
 }
