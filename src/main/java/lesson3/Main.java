5c3d30339e614858c557a5ce26fb17217ccf4598
OGMOL <olegmolokov11@gmail.com>
2024-11-03 17:03:29 +0300

    Task1 completed

---
+++ src/main/java/lesson3/Main.java
@@ -0,0 +1,14 @@
+package lesson3;
+
+public class Main {
+    public static void main(String[] args) {
+        printThreeWords();
+    }
+
+    /**
+     * Метод выводит три слова в столбец.
+     */
+    public static void printThreeWords() {
+        System.out.println("Orange" + "\n" + "Banana" + "\n" + "Apple");
+    }
+}
