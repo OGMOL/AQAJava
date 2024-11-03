b64189c2b368c89edbf8dc890ab8f19da78bdae8
OGMOL <olegmolokov11@gmail.com>
2024-11-03 22:56:56 +0300

    task13 completed

--- src/main/java/lesson3/Main.java
+++ src/main/java/lesson3/Main.java
@@ -15,6 +15,7 @@ public class Main {
         replaceZerosAndOnes();
         fillArray();
         multiplyingLessThenSixArray();
+        fillDiagonal();
     }
 
     /**
@@ -172,5 +173,26 @@ public class Main {
         }
 
         System.out.println(Arrays.toString(array));
+    }
+
+    /**
+     * Task13
+     * Метод создает квадратный двумерный целочисленный массив и заполняет диагональные элементы единицами.
+     */
+    public static void fillDiagonal() {
+        int size = 5;
+        int[][] array = new int[size][size];
+
+        for (int i = 0; i < size; i++) {
+            array[i][i] = 1;
+            array[i][size - 1 - i] = 1;
+        }
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
